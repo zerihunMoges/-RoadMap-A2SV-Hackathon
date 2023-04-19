7ba1e2dcf8fa5fd6000a9bcef06357a5028d82ac
unknown <yaredt29@gmail.com>
2023-04-19 20:36:01 +0300

    feat(web): fixing rebase conflicts

--- roadmap/pages/_app.js
+++ roadmap/pages/_app.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-
+import { AuthContextProvider } from '../context/AuthContext'
 import createEmotionCache from '../utility/createEmotionCache';
 import lightTheme from '../styles/theme/lightTheme';
-import '../styles/globals.css';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -12,12 +11,14 @@ const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <AuthContextProvider>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+     </AuthContextProvider>
   );
 };
 
