1ee6f8a10ac587a7266b2ab86433a770df0271ed
unknown <yaredt29@gmail.com>
2023-04-19 23:18:00 +0300

    feat(web): modify the roadmap detail page

--- roadmap/components/RoadMapDetail.js
+++ roadmap/components/RoadMapDetail.js
@@ -5,91 +5,62 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
 import { deepOrange } from '@mui/material/colors';
 import { IconButton, List, ListItem, ListItemAvatar, ListItemIcon, ListItemText, ListSubheader, Rating, Switch } from '@mui/material';
-import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import PitstopCard from './PitstopCard';
+import { BorderAllRounded } from '@mui/icons-material';
 
 function RoadMapDetail() {
+  const data = [{title:"the title of the pitstop",  duration:380,}, {title:"Real life is a blessing",  duration:380,},{title:"Not if they fucking for Nyash",  duration:380,}]
   return (
-    <div>
-<div style={{display:'flex', flexDirection: 'row', maxHeight: 400}}>
-        <div style={{padding: 10, marginTop: 'auto', marginBottom: 'auto'}}>
-        <Card sx={{ border: "none", boxShadow: "none" }}>
-      <CardContent>
-        
-        <Typography variant="h5" component="div">
-        Google Project Management: Professional Certificate
+    <div style={{marginRight: "13%", marginLeft: "13%"}}>
+
+      <div style={{ display:"flex", flexDirection:"column", gap:"20px", alignItems:'center', paddingTop: 40}}>
+        <Typography variant="h4" component="h2" fontWeight="bold" color="text.secondary">
+            Google Project Management: Professional Certificate
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Start your path to a career in project management. In this program, you’ll learn in-demand skills that will have you job-ready in less than six months. No degree or experience is required.
-        </Typography>
+        <img style={{ width: 600, objectFit: 'contain'}} src='https://img.freepik.com/free-photo/virtual-classroom-study-space_23-2149178591.jpg?w=2000'></img>
         <div style={{ display: 'flex'}}>
-          <Rating name="size-small" defaultValue={2}  size="small" /> 
-          <Typography sx={{ fontSize: 14, marginLeft: 2 }} color="text.secondary" gutterBottom>
-          Based on 5000 users
-        </Typography>
-        </div>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Join The Journey</Button>
-      </CardActions>
-    </Card>
-        </div>
-        <div style={{width: '50%', paddingTop: 10}}>
-        <img style={{ width: 400, height: 500, objectFit: 'contain'}} src='https://img.freepik.com/free-photo/virtual-classroom-study-space_23-2149178591.jpg?w=2000'></img>
-        </div>
+            <Rating name="size-small" defaultValue={2}  size="medium" /> 
+            <Typography sx={{ fontSize: 12, marginLeft: 1, marginTop:0.6 }} color="text.secondary" gutterBottom>
+              Based on 5000 users
+            </Typography>
+          </div>
+      </div>
+      <hr style={{width: '100%', marginTop:"3rem"}}></hr>
 
-        </div>
+      <div style={{display: 'flex', flexDirection: 'row', gap:"80px", width:'100%'}}>
+          <div style={{flexBasis: '60%'}}>
 
-        <hr style={{width: '98%'}}></hr>
+          <Typography variant="h5" component="div" color="text.secondary">
+            Description
+          </Typography>
+          <Typography sx={{ marginTop: 3 }} color="text.secondary">
+            Get started in the in-demand field of project management with a Professional Certificate from Google. Learn how to manage projects using traditional and agile methods, create project documentation, and develop strategic communication skills.
+          </Typography>
+          {
+            data.map((lecture, idx) => {
+              return(<PitstopCard key={idx} data={lecture}/>)
+            })
+          }
 
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-            <div style={{padding: 15, marginTop: 10, width: '60%'}}>
-
-            <Typography variant="h5" component="div">
-        Description:
-        </Typography>
-        <Typography sx={{ mb: 1.5, marginTop: 5 }} color="text.secondary">
-        Get started in the in-demand field of project management with a Professional Certificate from Google. Learn how to manage projects using traditional and agile methods, create project documentation, and develop strategic communication skills.
-        </Typography>
-
-            <List
-      sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}
-      
-    >
-      <ListItem>
-        <ListItemIcon>
-        <PlayCircleFilledWhiteIcon></PlayCircleFilledWhiteIcon>
-        </ListItemIcon>
-        <ListItemText id="switch-list-label-wifi" primary="Introduction to Google Project Management" />
-        
-      </ListItem>
-      <hr style={{width: '95%'}}></hr>
-      <ListItem>
-        <ListItemIcon>
-        <PlayCircleFilledWhiteIcon></PlayCircleFilledWhiteIcon>
-        </ListItemIcon>
-        <ListItemText id="switch-list-label-wifi" primary="Introduction to Google Project Management" />
-        
-      </ListItem>
-      <hr style={{width: '95%'}}></hr>
-      <ListItem>
-        <ListItemIcon>
-        <PlayCircleFilledWhiteIcon></PlayCircleFilledWhiteIcon>
-        </ListItemIcon>
-        <ListItemText id="switch-list-label-wifi" primary="Introduction to Google Project Management" />
-        
-      </ListItem>
-      <hr style={{width: '95%'}}></hr>
-      </List>
-
-            </div>
-            <div>
+          </div>
+          <div style={{flexBasis:'40%'}}>
+            <Card sx={{ border: "none", boxShadow: "none" }}>
+              <CardContent>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Start your path to a career in project management. In this program, you’ll learn in-demand skills that will have you job-ready in less than six months. No degree or experience is required.
+                </Typography>
                 
-            </div>
-        </div>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" color='secondary'  size="large">Enroll</Button>
+              </CardActions>
+            </Card>
+          </div>
+      </div>
+
+
     </div>
   )
 }
