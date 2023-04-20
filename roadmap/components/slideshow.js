7d69455212a7ca6a03fdc0d41111af5cf40ea0bb
zerabruckmuluneh <zerabruck@a2sv.org>
2023-04-20 05:14:48 +0300

    home page updated

--- roadmap/components/slideshow.js
+++ roadmap/components/slideshow.js
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import RoadMap from './RoadMap';
 
 export default function SlideShow({cardData}){
 
     return <div container style={{ overflowX: 'scroll',overflowY: 'hidden',  display: 'flex'}}>
-  {cardData.map((card, index) => (
-    <RoadMap data={card} key={index}></RoadMap>
+  {cardData.map((single) => (
+    <Link style={{textDecoration: "none"}} href="/roadmap/[id]" as={`/roadmap/${single.id}`}>
+    <RoadMap data={single}/>
+    </Link>
   ))}
 </div>
 
--- roadmap/pages/index.js
+++ roadmap/pages/index.js
@@ -7,11 +7,13 @@ import IconBox from "@/components/iconbox";
 import { Computer as ComputerIcon } from '@material-ui/icons';
 import { School as SchoolIcon } from '@material-ui/icons';
 import { data } from "../mock";
+import { useRouter } from "next/router";
 
 const StyledLink = styled("a")({
   textDecoration: "none"
 });
 export default function HomePage() {
+  const router = useRouter()
   return (
     <div style={{marginRight:"10%", marginLeft:"10%"}}>
     <Box maxWidth="1500px" mx="auto" marginTop='100px' >
@@ -21,32 +23,28 @@ export default function HomePage() {
           maxWidth="800px"
         >
           <Typography fontWeight="bold" variant="h3">
-            <span style={{ color: "rebeccapurple" }}>Move faster</span> with
-            intuitive React UI tools
+            <span style={{ color: "rebeccapurple" }}>Discover</span> your potential with our guided course roadmap.
           </Typography>
           
           <Typography
             marginTop={12}
             variant="body1"
-            style={{ marginBottom: "24px" }}
+            style={{ marginBottom: "24px" ,marginTop:"10px" , color:"#9CA0A4"}}
           >
-            MUI offers a comprehensive suite of UI tools to help you ship new
-            features faster. Start with Material UI, our fully-loaded component
-            library, or bring your own design system to our production-ready
-            components.
+            We help students choose the right course with a personalized roadmap to success, offering valuable resources and support for informed decision-making.
           </Typography>
           <Box display="flex" marginTop={8}>
             <Box style={{ marginRight: "16px" }}>
-              <Typography
+              {/* <Typography
                 variant="h4"
                 fontWeight="bold"
                 style={{ color: "rebeccapurple" }}
-              >
-                30M
-              </Typography>
-              <Typography variant="caption">Students learning</Typography>
+              > */}
+                {/* 30M */}
+              {/* </Typography> */}
+              {/* <Typography variant="caption">Students learning</Typography> */}
             </Box>
-            <Box style={{ marginRight: "16px" }}>
+            {/* <Box style={{ marginRight: "16px" }}>
               <Typography
                 variant="h4"
                 fontWeight="bold"
@@ -55,8 +53,8 @@ export default function HomePage() {
                 300M+
               </Typography>
               <Typography variant="caption">Oppotunities got</Typography>
-            </Box>
-            <Box style={{ marginRight: "16px" }}>
+            </Box> */}
+            {/* <Box style={{ marginRight: "16px" }}>
               <Typography
                 variant="h4"
                 fontWeight="bold"
@@ -65,12 +63,15 @@ export default function HomePage() {
                 400+
               </Typography>
               <Typography variant="caption">Groups created</Typography>
-            </Box>
+            </Box> */}
           </Box>
 
           <Box display="flex" marginTop={8}>
             <StyledLink href="/material-ui/getting-started/overview/">
+              <a href="#about">
               <CustomButton text="Know More"></CustomButton>
+              </a>
+              
             </StyledLink>
           </Box>
         </Box>
@@ -91,10 +92,13 @@ export default function HomePage() {
 
       <Box display="flex" justifyContent="space-between" marginTop={10}>
         <Typography fontWeight="bold" variant="h3">
-          <span style={{ color: "rebeccapurple" }}>Popular</span> Courses
+          <span style={{ color: "rebeccapurple" }}>Popular</span> Roadmaps
         </Typography>
+        <div onClick={()=>router.push('/roadmaps')}>
+        <CustomButton text="All Roadmaps"></CustomButton>
+        </div>
 
-        <CustomButton text="All Courses"></CustomButton>
+        
       </Box>
       <SlideShow cardData={data}></SlideShow>
       <Box display="flex" marginTop={12}>
@@ -103,7 +107,7 @@ export default function HomePage() {
           maxWidth="800px"
         >
           <Typography fontWeight="bold" variant="h3">
-            <span style={{ color: "rebeccapurple" }}>Best Speciality</span> about
+            <span id="about" style={{ color: "rebeccapurple" }}>Best Speciality</span> about
             our platform
           </Typography>
           <Box marginTop={6}></Box>
@@ -112,30 +116,18 @@ export default function HomePage() {
             variant="body1"
             style={{ marginBottom: "24px" }}
           >
-            MUI offers a comprehensive suite of UI tools to help you ship new
-            features faster. Start with Material UI, our fully-loaded component
-            library, or bring your own design system to our production-ready
-            components.
+            Our website is a comprehensive educational platform designed to help students navigate the complexities of choosing the right course. We offer a personalized roadmap to success, guiding students through the best course options based on their interests, goals, and career aspirations.
           </Typography>
       
           <Box container marginTop={6}>
-            <IconBox icon={<ComputerIcon></ComputerIcon>} text1='UI tools to help you ship new
-            features faster. Start with Material UI' text2='UI tools to help you ship new
-            features faster. Start with Material UI' text3='features faster. Start with Material UI'></IconBox>
+            <IconBox icon={<ComputerIcon></ComputerIcon>} text1='Personalized Course Roadmap' text2='Our platform offers a tailored course roadmap that helps students choose the right course based on their interests, goals, and career aspirations.'></IconBox>
             <Box marginTop={6}></Box>
-            <IconBox icon={<SchoolIcon></SchoolIcon>} text1='UI tools to help you ship new
-            features faster. Start with Material UI' text2='UI tools to help you ship new
-            features faster. Start with Material UI' text3='features faster. Start with Material UI'></IconBox>
+            <IconBox icon={<SchoolIcon></SchoolIcon>} text1='Resources and Support' text2='We provide students with a wealth of resources and support, including expert advice, informational articles, and tools to help them make informed decisions about their education.'></IconBox>
             <Box marginTop={6}></Box>
-            <IconBox icon={<TuneIcon></TuneIcon>} text1='UI tools to help you ship new
-            features faster. Start with Material UI' text2='UI tools to help you ship new
-            features faster. Start with Material UI' text3='features faster. Start with Material UI'></IconBox>
+            <IconBox icon={<TuneIcon></TuneIcon>} text1='Course Comparison and Reviews' text2=' Our platform allows students to compare courses and read reviews from other students, helping them make well-informed decisions about their education.'></IconBox>
           </Box>
 
           <Box display="flex" marginTop={6}>
-            <StyledLink href="/material-ui/getting-started/overview/">
-              <CustomButton text="Know More"></CustomButton>
-            </StyledLink>
           </Box>
         </Box>
       
